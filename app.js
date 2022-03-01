ab83d9411f17f480b0d2bc113853be98e3aa4686
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-01 08:22:12 -0600

    pseudocode

---
+++ app.js
@@ -0,0 +1,7 @@
+// Prompt user for manager info (based on Employee class)
+// then show the 'engineer, intern, or end' menu
+    // if engineer, show prompts for engineer class
+        // then back to menu
+    // if intern, show prompts for intern class
+        // then back to menu
+    // if end, render html and exit program
